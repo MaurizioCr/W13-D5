4e588ac16ce667bdde75104a613583bebced86ff
MaurizioCr <crispinomaurizio034@gmail.com>
2023-12-01 11:40:05 +0100

    Metodo play

---
+++ src/elementoMultimediale.java
@@ -0,0 +1,14 @@
+public class elementoMultimediale {
+    String titolo;
+    int minutaggio;
+
+    public elementoMultimediale(String titolo, int minutaggio){
+        this.titolo=titolo;
+        this.minutaggio=minutaggio;
+    }
+    public void play(){
+        if (this.minutaggio>0){
+        System.out.println("***** Hai premuto play *****");
+        }
+    }
+}
